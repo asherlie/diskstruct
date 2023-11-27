156f4c63fd65bc9065d84e1e93b6fb845b8da34f
asherlie <lieber.asher@gmail.com>
2023-11-27 00:01:53 -0500

    updated implementation notes

--- map.h
+++ map.h
@@ -39,6 +39,42 @@ struct bucket{
      *
      * if an entry must be inserted into a bucket and n_entries == capacity, ftruncate
      * will be used to grow the bucket file by at least element_sz in order to accomodate a new entry
+     *
+     * two file pointers will be store for each bucket
+     *  one to keep track of the header information
+     *  
+     *  one pointing to the next available offset for writing an entry, nvm actually, this won't be compatible with atomic
+     *  threadsafety
+     *
+     * just the one at header will be needed
+     * multiple threads will be able to write concurrently by opening a FP right before writing after calling
+     * atomic_increment
+     *
+     * the only time insertions will NOT be atomic will be when resizing is needed
+     * in this case, a lock will be acquired and the bucket in question will be ftruncate()d
+     * how do i ensure that all writes in progress have finished before acquiring lock and ftruncate()ing?
+     * i think i'll be able to increment an atomic int each time we open an entry-adding FP
+     * and decrement the same counter after a write is completed
+     *
+     * hmm, but a lock is only useful if it's acquired before each atomic write which defeats the purpose of atomic writes
+     * pthread_mutex_
+     *
+     * okay, we likely don't even need a mutex lock actually
+     *  safety can be achieved by using the aforementioned atomic counter
+     *  if the counter is 0, indicating that no other thread is inserting into this bucket, we know that no other thread will
+     *  attempt to insert until we have more available bytes for entry
+     *  with this knowledge we can ignore the problem of insertions during resizing
+     *
+     *  however this still does leave the problem of multiple threads attempting to resize using ftruncate() simultaneously
+     *  this can be solved with a mutex lock or a separate atomic variable
+     *  this atomic variable will be a boolean - resizing_in_progress
+     *
+     *
+     * we could maybe skip this whole mess by simply putting the responsibility of resizing buckets on the thread who
+     * got the value of capacity from atomic_increment(n_entries)
+     * we may need to do something similar anyway in the event that
+     *
+     *
      */
 };
 
