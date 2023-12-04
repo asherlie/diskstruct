00c3f70e9cc8b9f6e767656dec7c9c46c767a633
asherlie <lieber.asher@gmail.com>
2023-12-03 22:21:33 -0500

    removed unused variable definitions in insert_map(), began implementing insert_map()

--- map.c
+++ map.c
@@ -13,13 +13,15 @@ void create_bucket_file(struct bucket* b){
 
 _Bool grow_file(char* fn, uint32_t grow_to){
     FILE* fp = fopen(fn, "w");
-    ftruncate(fileno(fp), grow_to);
+    return !ftruncate(fileno(fp), grow_to);
+
 }
 
 void init_map(struct map* m, char* name, uint16_t n_buckets, uint32_t key_sz, uint32_t value_sz, 
               char* bucket_prefix, uint16_t (*hashfunc)(void*)){
-    FILE* tmp_bucket;
-    uint32_t entrysz = key_sz + value_sz;
+    /*FILE* tmp_bucket;*/
+    /*uint32_t entrysz = key_sz + value_sz;*/
+
     strcpy(m->name, name);
     strcpy(m->bucket_prefix, bucket_prefix);
     m->hashfunc = hashfunc;
@@ -37,6 +39,7 @@ void init_map(struct map* m, char* name, uint16_t n_buckets, uint32_t key_sz, ui
         */
         m->buckets[i].cap = 0;
         m->buckets[i].n_entries = 0;
+        m->buckets[i].insertions_in_prog = 0;
         /*tmp_bucket = fopen(m->buckets[i].fn, "w");*/
         /*m->buckets[i].cap = 1;*/
         /*ftruncate(fileno(tmp_bucket), entrysz);*/
@@ -45,10 +48,38 @@ void init_map(struct map* m, char* name, uint16_t n_buckets, uint32_t key_sz, ui
 /* loads map into memory */
 void load_map(struct map* m, char* name, uint32_t key_sz, uint32_t value_sz, char* bucket_prefix);
 /* k/v size must be consistent with struct map's entries */
-_Bool insert_map(struct map* m, void* key, void* value){
+int insert_map(struct map* m, void* key, void* value){
     uint16_t idx = m->hashfunc(key) % m->n_buckets;
+    uint32_t bucket_idx, bucket_cap;
     struct bucket* b = &m->buckets[idx];
+    int retries = -1;
 
+
+/*
+ *     lookup bucket, grab idx, grow_file() if needed, retry if idx > cap
+ *     increment insertions_in_prog, insert, decrement insertions_in_prog
+ *     exit
+ * 
+ *     is it possible that 2 threads will resize simultaneously?
+ *     one thread resizes, every other thread gets idx > cap, retries UNTIL cap is updated
+ *     so i gues cap should be atomically loaded! right after retry:
+*/
+    bucket_idx = atomic_fetch_add(&b->n_entries, 1);
+    /* retrying after getting idx for now
+     * this way we never have a corrupted n_entries and we can always set it
+     * using the above - eventually we'll either reach a point where idx == cap
+     * or where we can insert if we keep retrying!
+     */
+    retry:
+    ++retries;
+    bucket_cap = atomic_load(&b->cap);
+    /* retry */
+    if (bucket_idx > bucket_cap) {
+        goto retry;
+    }
+    /* resize bucket */
+    if (bucket_idx == bucket_cap) {
+    }
     /*
      * right now i'm assuming that we need to initialize buckets to nonzero size
      * but it's probably alright to start them at 0 size as well
@@ -58,7 +89,7 @@ _Bool insert_map(struct map* m, void* key, void* value){
      *     the first insertion thread will get idx == cap upon calling atomic_inc!
      *     no special case needed :)
     */
-    
+    return retries;
 }
 void* lookup_map(struct map* m, void* key);
 
